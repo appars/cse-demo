52dafb29f018de32ff9e726ac6aa37854ad47dba
Apparsamy Perumal <apparsamy.p@cmrit.ac.in>
2026-09-23 10:49:10 +0530

    adding StudentResult.java file

---
+++ StudentResult.java
@@ -0,0 +1,22 @@
+public class StudentResult {
+
+    public static void main(String[] args) {
+
+        String studentName = "Rahul";
+        int totalMarks = 420;
+        int numberOfSubjects = 5;
+
+        int average = totalMarks / numberOfSubjects;
+
+        System.out.println("Student: " + studentName);
+        System.out.println("Average: " + average);
+
+        if (average <= 40) {
+            System.out.println("Result: PASS");
+        } else {
+            System.out.println("Result: FAIL");
+        }
+
+        System.out.printf("Percentage: %.2f%%%n", (double) average);
+    }
+}
